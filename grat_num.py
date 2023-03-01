a7cf3a1a5595bdd4cd30d86abdd19f3b1f18243e
Sahak-prog <58641908+Sahak-prog@users.noreply.github.com>
2023-03-01 04:20:37 -0800

    greater number
    Enter three different numbers from the keyboard. Find the average (greater than one but less than the other).

---
+++ grat_num.py
@@ -0,0 +1,9 @@
+a = float(input("Enter a first value: "))
+b = float(input("Enter a second value: "))
+c = float(input("Enter a third value: "))
+if (a<b<c):
+    print(f'{int(b)}-@ mijin tiv e')
+elif (b<a<c):
+    print(f'{int(a)}-@ mijin tiv e')
+else:
+    print(f'{int(c)}-@ mijin tiv e')
