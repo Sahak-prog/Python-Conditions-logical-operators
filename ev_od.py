5dfc096c16d1a0f1b2e5a28478e81da11fb7abf9
Sahak-prog <58641908+Sahak-prog@users.noreply.github.com>
2023-03-01 04:09:22 -0800

    even and odd
    Enter two random numbers from the keyboard, one even and the other odd. Decide and display an odd number.

---
+++ ev_od.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a first value: "))
+b = int(input("Enter a second value: "))
+if (a % 2) == 0:
+    print("{0} is Even".format(a))
+else:
+    print("{0} is Odd".format(a))
+if (b % 2) == 0:
+    print("{0} is Even".format(b))
+else:
+    print("{0} is Odd".format(b))
