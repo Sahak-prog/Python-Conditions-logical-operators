03e7807d94d8ab3e098994ccc57e02a0736ee65f
Sahak-prog <58641908+Sahak-prog@users.noreply.github.com>
2023-03-01 04:21:42 -0800

    ASCII
    Enter an integer representing the character code according to the ASCII table. Determine if this is an English letter code or some other character.

---
+++ ASCII.py
@@ -0,0 +1,7 @@
+value_char = int(input("Enter a value: "))
+if (value_char >= 65 and value_char <= 90):
+    print("Anglereni mecatareri mej e")
+elif (value_char >= 97 and value_char <= 122):
+    print("Anglereni poqratareri mej e")
+else:
+    print("chi patkanum")
